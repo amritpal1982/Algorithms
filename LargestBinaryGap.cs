f8041f0e4450e0071f2d2c7a67d0e6c7c64813de
Amritpal Singh <amritpal82@gmail.com>
2017-04-26 22:55:28 +0530

    Removed unused namespaces

--- LargestBinaryGap.cs
+++ LargestBinaryGap.cs
@@ -1,10 +1,5 @@
-//Rextester.Program.Main is the entry point for your code. Don't change it.
-//Compiler version 4.0.30319.17929 for Microsoft (R) .NET Framework 4.5
-
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
 
 namespace Algorithms
 {

f1cbb8344dcb1d149574c1384d0983d0b10ea5c9
Amritpal Singh <amritpal82@gmail.com>
2017-05-16 14:31:30 +0530

    Fixed the whitespace and indentation

--- Square Root (Babylonian Method).cs
+++ Square Root (Babylonian Method).cs
@@ -4,43 +4,43 @@ using System.Collections.Generic;
 
 namespace Algorithms
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            //Your code goes here
-            double sqrt = SquareRoot(128.8976);//128.8976
-            Console.WriteLine(sqrt);
-        }
-        
-        public static double SquareRoot(double number)
-        {
-            //start with a random number. We can find a method to find this initial number to be close to actual square root.
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			//Your code goes here
+			double sqrt = SquareRoot(128.8976);//128.8976
+			Console.WriteLine(sqrt);
+		}
+		
+		public static double SquareRoot(double number)
+		{
+			//start with a random number. We can find a method to find this initial number to be close to actual square root.
 			//But for the sake of simplicity, I am starting it with 1, here
 			double result = 1.0;
-            int iteration = 0;
-            
+			int iteration = 0;
+			
 			//In the decimal numbers, instead of checking for equality, we need to check for the gap of the result, upto a certain decimal places.
 			double approximation = 0.0000000000001;
-            double iterationResult;
-            do
-            {
-                //Check if the number when divided by current result yields the same result
+			double iterationResult;
+			do
+			{
+				//Check if the number when divided by current result yields the same result
 				//For example 15/3.8762 = 3.8762 
 				//In other words 15/3.8762 - 3.8762 = 0.0 or 0.000000000001
 				//If so, we have found the square root, so break; 
 				if(Math.Abs((number/result) - result) <= approximation)
-                {
-                    break;
-                }
+				{
+					break;
+				}
 				
 				//if not, then find the next approximation, by averaging the last yield with its result.
-                result = ((number/result) + result)/2;
-                iteration++;
-            }while(true);
-            
-            Console.WriteLine("Iterations: " + iteration);
-            return result;
-        }
-    }
+				result = ((number/result) + result)/2;
+				iteration++;
+			}while(true);
+			
+			Console.WriteLine("Iterations: " + iteration);
+			return result;
+		}
+	}
 }
